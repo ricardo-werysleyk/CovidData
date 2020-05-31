b0cbf901ccc6bff7a24bd2734951f4045d6ed91e
Ricardo Werysleyk <werysleyk.ufrn@gmail.com>
2020-05-31 03:47:09 -0300

    Layout do Site

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,19 @@
-var casos, deaths, recovered;
-var world = [];
+//Variaveis de dados
+var country = [], cases = [], todayCases = [], deaths = [],todayDeaths = [], recovered = [], active = [], critical = [];
+var casesPerOneMillion = [], deathsPerOneMillion = [], totalTests = [];
+var dadosApi = [];
+var done = false;
+var attDados;
+var tempoCD = 1000;
+
+//Variaveis dos elementos HTML
+var inputCasos = document.getElementById('inputCasos');
 
 
 carregaApi();
+// attDados = setInterval(() => {
+//     carregaVariaveis();
+// }, tempoCD);
 
 function carregaApi(){
     fetch('https://coronavirus-19-api.herokuapp.com/countries')
@@ -11,14 +22,31 @@ function carregaApi(){
             return response.json();
         })
         .then((data) => {
-            world = data;
-            console.log(world)
+            dadosApi = data;
+            done = true;
         })
         .catch((erro) => {
             console.error(erro);
         })
 }
 
-function casos(){
-    console.log(world[0].todayCases);
+function carregaVariaveis(){
+    if(done){
+        for(var i in dadosApi){
+            country[i] = dadosApi[i].country;
+            cases[i] = dadosApi[i].cases;
+            todayCases[i] = dadosApi[i].todayCases;
+            deaths[i] = dadosApi[i].deaths;
+            todayDeaths[i] = dadosApi[i].todayDeaths;
+            recovered[i] = dadosApi[i].recovered;
+            active[i] = dadosApi[i].active;
+            critical[i] = dadosApi[i].critical;
+            casesPerOneMillion[i] = dadosApi[i].casesPerOneMillion;
+            deathsPerOneMillion[i] = dadosApi[i].deathsPerOneMillion;
+            totalTests[i] = dadosApi[i].totalTests;
+        }
+        console.log('done');
+    }else{
+        console.log('Requisição em andamento!')
+    }
 }
